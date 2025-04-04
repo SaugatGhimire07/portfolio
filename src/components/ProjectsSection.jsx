ba077e130cf1bbd6af0f424a13696efabcb73e71
SaugatGhimire07 <Ghimiresaugat987@gmail.com>
2025-04-04 15:18:24 -0400

    update ProjectsSection link to new Spotify Profile application URL

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -52,7 +52,7 @@ const ProjectsSection = () => {
             <ProjectItem
               title="Spotify Profile"
               description="Web app for visualizing personalized Spotify data. View your top artists, top tracks, recently played tracks, and detailed audio information about each track. Create and save new playlists of recommended tracks based on your existing playlists and more."
-              link="https://spotify-profile-hazel.vercel.app/dashboard"
+              link="https://spotify.saugat.dev/"
               imageProps={{
                 alt: "Spotify Profile application",
                 src: spotifyProfileImage,
